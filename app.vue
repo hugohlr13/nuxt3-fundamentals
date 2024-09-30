426a4f4e26f47f141f0b4adf50df1c0af6e437c2
Hugo Huet-Leroy <hugohuetleroy@hugosmacstudio.home>
2024-09-30 18:58:06 +0200

    routing in nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nav style="display: flex; gap: 10px;" >
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io/">Vue School</NuxtLink>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/movies/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Page: Movies
+  </div>
+</template>
+
+<style scoped></style>
