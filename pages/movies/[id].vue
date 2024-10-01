e608433977a1e67c4044511deb41f0c1bf403f29
Hugo Huet-Leroy <hugohuetleroy@hugosmacstudio.home>
2024-10-01 17:57:31 +0200

    error fetch

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,7 +1,25 @@
+<script setup>
+const route = useRoute();
+
+// {"Response":"False","Error":"Incorrect IMDb ID."}
+
+const { data } = await useFetch(
+  `http://www.omdbapi.com/?apikey=3bcd8b6&i=${route.params.id}`,
+  {
+    pick: ["Plot", "Title", "Error"],
+    key: `/movies/${route.params.id}`,
+  }
+);
+
+if (data.value.Error === "Incorrect IMDb ID.") {
+        showError({ statusCode: 404, statusMessage: "Page Not Found" });
+      }
+</script>
+
 <template>
-<div>
-    <h1>
-        {{$route.params.id}}
-    </h1>
-</div>
+  <div>
+    <pre>
+        {{ data }}
+    </pre>
+  </div>
 </template>
