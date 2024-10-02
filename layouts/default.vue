de9b9eb87b01aea71f934a665d6cba8211eec53c
Hugo Huet-Leroy <hugohuetleroy@hugosmacstudio.home>
2024-10-02 15:04:25 +0200

    useState

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
+<script setup>
+const isLoggedIn = useIsLoggedIn();
+</script>
 <template>
     <nav style="display: flex; gap: 10px;" >
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/movies">Movies</NuxtLink>
       <NuxtLink to="https://vueschool.io/">Vue School</NuxtLink>
+      <NuxtLink v-if="!isLoggedIn" to="/login">Login</NuxtLink>
+      <a href="#" v-else="isLoggedIn" @click.prevent="isLoggedIn= false">Logout</a> 
       Test
     </nav>
 <slot></slot>
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,17 @@
 <script setup>
+const isLoggedIn = useIsLoggedIn();
 definePageMeta({
     layout: "plain",
 })
+
+function login() {
+    isLoggedIn.value = true;
+    useRouter().push("/");
+}
 </script>
 
 <template>
-    <form>
+    <form @submit.prevent="login">
         <h1>Login</h1>
         <label>
             Username
@@ -15,5 +21,6 @@ definePageMeta({
             Password
             <input type="password" />
         </label>
+        <button>Login</button>
     </form>
 </template>
